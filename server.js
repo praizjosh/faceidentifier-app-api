3fca4eb4a389f1115ca9831545aee2207e9ac900
praizjosh <40269551+praizjosh@users.noreply.github.com>
2021-01-26 15:12:32 +0100

    port changed again

--- server.js
+++ server.js
@@ -60,8 +60,8 @@ app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3001, () => {
-	console.log(`App is running on port ${process.env.PORT}`);
+app.listen(3001, () => {
+	console.log('App is running on port 3001');
 })
 
 /*
